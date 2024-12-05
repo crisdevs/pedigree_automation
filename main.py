33940a1a0ab604a05aba6e95941da35cf42d43dc
Cristian Corrales <crisdevs95@gmail.com>
2024-12-05 15:53:35 -0500

    Restored branch to original state and created seperate branch for the gui version

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from API_KEYS import api_keys, CSE_ID
 from tv_dictionary import brand_logic
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.common.keys import Keys
 from openai import OpenAI
 
 from urllib.parse import urlparse
@@ -22,7 +22,7 @@ def insertMultiple(arr, index, val, quantity):
   for i in range(quantity):
     arr.insert(index, val)
 
-# #Function for doing the google search.
+#Function for doing the google search.
 def google_search(query, api_key, cse_id):
     url = 'https://www.googleapis.com/customsearch/v1'
     params = {
@@ -72,53 +72,53 @@ def get_url(brand, model):
                       Prioritize website links from the manufacturer website first and if you cannot find the 
                       website url from the manufacturer then prioritize URLs from best buy.'
                       """)
-    # isCorrectURL = input(f'Is this the correct URL?(Y/N):{url}\n')
-    # if isCorrectURL.lower() == 'no' or isCorrectURL.lower() == 'n':
-    #     url = input('Please enter correct url\n')
+    isCorrectURL = input(f'Is this the correct URL?(Y/N):{url}\n')
+    if isCorrectURL.lower() == 'no' or isCorrectURL.lower() == 'n':
+        url = input('Please enter correct url\n')
     return url
-# #This is to format the response from chatgpt to have answers in the right place in the excel workbook
-# def format_pedigree_answer(answer):
-#   #Seperates pedigree answers into a list
-#   pedigree_specs_arr = answer.split(",")
-#   final_pedigree_specs_arr = []
-#   #This was a quick way I found to remove spaces found at the start of each of the answers returned from chatgpt
-#   for i in pedigree_specs_arr:
-#         #If the first character was a space in the list
-#         if i[0] == " ":
-#             #Append the character after the space in the new list which should be the actual start of the answer
-#             final_pedigree_specs_arr.append(i[1:])
-#         #If the first character was not a space then just add to the final list
-#         else:
-#             final_pedigree_specs_arr.append(i)
-#   #Add multiple spaces so that answers will be under the right question in excel
-#   insertMultiple(final_pedigree_specs_arr, 1, " ", 2)
-#   insertMultiple(final_pedigree_specs_arr, 4, " ", 2)
-#   insertMultiple(final_pedigree_specs_arr, 19, " ", 11)
-#   insertMultiple(final_pedigree_specs_arr, 31, " ", 5)
-#   #Make the list into a string again but seperated by tabs(cells) for excel
-#   final_pedigree_specs = "\t".join(final_pedigree_specs_arr)
+#This is to format the response from chatgpt to have answers in the right place in the excel workbook
+def format_pedigree_answer(answer):
+  #Seperates pedigree answers into a list
+  pedigree_specs_arr = answer.split(",")
+  final_pedigree_specs_arr = []
+  #This was a quick way I found to remove spaces found at the start of each of the answers returned from chatgpt
+  for i in pedigree_specs_arr:
+        #If the first character was a space in the list
+        if i[0] == " ":
+            #Append the character after the space in the new list which should be the actual start of the answer
+            final_pedigree_specs_arr.append(i[1:])
+        #If the first character was not a space then just add to the final list
+        else:
+            final_pedigree_specs_arr.append(i)
+  #Add multiple spaces so that answers will be under the right question in excel
+  insertMultiple(final_pedigree_specs_arr, 1, " ", 2)
+  insertMultiple(final_pedigree_specs_arr, 4, " ", 2)
+  insertMultiple(final_pedigree_specs_arr, 19, " ", 11)
+  insertMultiple(final_pedigree_specs_arr, 31, " ", 5)
+  #Make the list into a string again but seperated by tabs(cells) for excel
+  final_pedigree_specs = "\t".join(final_pedigree_specs_arr)
   
-#   return final_pedigree_specs
+  return final_pedigree_specs
 
-# def main():
-#   #Pedigree questions are held in a seperate file that is not tracked by Git.
-#   pedigree_questions = None
-#   with open('./files/pedigree_questions.txt', 'r') as file:
-#       pedigree_questions = file.read()
-#   file.close()
-#   product = input("Please enter brand and model\n")
-#   #Grabs the brand portion of the string entered by the user
-#   brand = product.split()[0].lower()
-#   #Does the same as above but for the model
-#   model = product.split()[1]
-#   product_link = get_url(brand, model)
-#   #This was a quick and easy way to get the domain name in the URL
-#   domain = urlparse(product_link).netloc
-#   if domain == 'www.bestbuy.com':
-#       brand = 'bestbuy'
-#   elif domain == 'www.lg.com':
-#       #Have to add #pdp_specs to the URL so that it automatically takes us to the specs section
-#       product_link += '#pdp_specs'
+def main():
+  #Pedigree questions are held in a seperate file that is not tracked by Git.
+  pedigree_questions = None
+  with open('./files/pedigree_questions.txt', 'r') as file:
+      pedigree_questions = file.read()
+  file.close()
+  product = input("Please enter brand and model\n")
+  #Grabs the brand portion of the string entered by the user
+  brand = product.split()[0].lower()
+  #Does the same as above but for the model
+  model = product.split()[1]
+  product_link = get_url(brand, model)
+  #This was a quick and easy way to get the domain name in the URL
+  domain = urlparse(product_link).netloc
+  if domain == 'www.bestbuy.com':
+      brand = 'bestbuy'
+  elif domain == 'www.lg.com':
+      #Have to add #pdp_specs to the URL so that it automatically takes us to the specs section
+      product_link += '#pdp_specs'
 
   #Get the latest driver of Chrome so that it does not need to be included with source code
 #   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
