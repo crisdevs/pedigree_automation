e03e6a2e3acc5ab88e73aa89e28f012b7b8b8509
Cristian Corrales <crisdevs95@gmail.com>
2025-02-05 13:36:09 -0500

    Created loading window

--- main.py
+++ main.py
@@ -7,10 +7,106 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from openai import OpenAI
-
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import tkinter as tk
 from urllib.parse import urlparse
 from webdriver_manager.chrome import ChromeDriverManager
+import asyncio
+import threading
 
+brands = [
+  'Samsung',
+  'LG',
+  'Vizio',
+  'TCL',
+  'Sony',
+  'Panasonic',
+  'Hisense',
+  'Roku',
+  'Toshiba'
+]
+
+tv_questions = ["Screen Size_in","Series","Super Series","Display Type","Minimum Width Of Stand","Miscellaneous In The Box","Native Resolution","Weight_lbs","Overall Width_in","Overall Height_in","Overall Depth_in","Panel Width_in",
+"Panel Height_in","Panel Depth_in","Claimed Has ATSC 3 or Nextgen TV","Claimed Has Variable Refresh Rate","FreeSync claim","G-Sync claim",	
+"Claimed Highest WiFi Standard","Model Year","Total Qty Of HDMI Inputs","Analog Connections","Stereo Audio Output","Digital Audio Output","Ethernet port",	
+"USB-A Ports","USB-C Ports","TV Has Built In Microphone","Can create individual user profiles","Has family settings/parental controls","Has Auto Low Latency Mode",
+"Has reduce blue light feature","Miracast","Airplay","Chromecast","Miscellaneous Features", "Website URL"]
+
+def loading_window(task_name, task, *args):
+    loading = tk.Toplevel(root)
+    loading.geometry('250x200')
+    
+    loading.columnconfigure(0, weight=1)
+    loading.rowconfigure(0, weight=1)
+    
+    loading_frame = tk.Frame(loading)
+    loading_frame.grid(row=0, column=0, sticky="nsew")
+    loading_frame.columnconfigure(0, weight=1)
+    loading_frame.rowconfigure(0, weight=1)
+    loading_frame.rowconfigure(1, weight=1)
+    
+    task_label = Label(loading_frame, text= 'Working on: ' + task_name)
+    task_label.grid(row=0, column=0, pady=(10 , 2), sticky= "nsew")
+    # task_label.rowconfigure(0, weight = 0)
+    # task_label.columnconfigure(0, weight=0)
+    progressbar = ttk.Progressbar(loading_frame, mode="indeterminate")
+    progressbar.grid(row=1, column=0, padx=20, sticky="new")
+    progressbar.start()
+    # results = await task(*args)
+    # return results
+    # task()
+    # def run_task():
+    #     loop = asyncio.new_event_loop()
+    #     asyncio.set_event_loop(loop)
+    #     results = loop.run_until_complete(task(*args))
+    #     loop.close()
+    #     loading.destroy()  # Close loading window after task completion
+    #     return results
+    threading.Thread(target=lambda:task(*args), daemon=True).start()
+    
+def get_url(brand, model):
+    product_type = 'TV'
+    query = f"{brand} {model}"
+    url  = None
+    # Will perform a google search using the brand and model entered by the user.
+    search_results = google_search(query, api_keys['google_search'], CSE_ID)
+    if not search_results:
+        url = input('URL not found please manually search and enter URL below\n')
+        return url
+    url = ask_chatgpt(f"""
+                      Here is some information I found on the web:\n{search_results}\n\n
+                      Now, based on this information, answer the following question: 
+                      'What is the product page link for the {brand} {model} {product_type} and only return the url. 
+                      Prioritize website links from the manufacturer website first and if you cannot find the 
+                      website url from the manufacturer then prioritize URLs from best buy.'
+                      """)
+    return url
+
+def searchWithBrandModel (brand, model):
+   url = get_url(brand, model)
+   
+   new_window = tk.Toplevel(root)
+   new_window.geometry('600x100')
+   new_window.title('URL Entry')
+   new_window.columnconfigure(0, weight=1)
+   new_window.rowconfigure(0, weight=1)
+
+   confirm_url_frame = tk.Frame(new_window)
+   confirm_url_frame.grid(row=0, column=0, sticky="nsew")
+   confirm_url_frame.columnconfigure(0, weight=1)
+   confirm_url_frame.rowconfigure(0, weight=1)
+   confirm_url_frame.rowconfigure(1, weight=1)
+   confirm_url_frame.rowconfigure(2, weight=1)
+
+   new_url_label = tk.Label(confirm_url_frame, text = "Confirm That this is the correct url you wish to grab specs from").grid(row=0, column=0)
+   new_url_text = tk.Entry(confirm_url_frame, width=70)
+   new_url_text.grid(row=1, column=0)
+   new_url_text.insert(0, url)
+   enter_url = tk.Button(confirm_url_frame, text = "Search", command=lambda:loading_window('Retrieving specs from model page.', main, brand, model, new_url_text.get(), new_window)).grid(row=2, column=0)
+   new_window.grab_set()
+    
 # #To help inserting values multiple times in the list.
 def insertMultiple(arr, index, val, quantity):
   for i in range(quantity):
@@ -34,10 +130,11 @@ def google_search(query, api_key, cse_id):
             #Each array contains a dictionary with the link property which we will append to the search_links list
             search_links.append(item['link'])
     return search_links
+
 #Function for asking chatGPT. Prompt is the prompt that you would like to ask ChatGPT
 def ask_chatgpt(prompt):
     client = OpenAI(api_key= api_keys['chat_gpt'])
-    completion = client.chat.completions.create(
+    completion =  client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
@@ -50,26 +147,7 @@ def ask_chatgpt(prompt):
     output = completion.choices[0].message.content
     return output
  
-def get_url(brand, model):
-    product_type = 'TV'
-    query = f"{brand} {model}"
-    url  = None
-    # Will perform a google search using the brand and model entered by the user.
-    search_results = google_search(query, api_keys['google_search'], CSE_ID)
-    if not search_results:
-        url = input('URL not found please manually search and enter URL below\n')
-        return url
-    url = ask_chatgpt(f"""
-                      Here is some information I found on the web:\n{search_results}\n\n
-                      Now, based on this information, answer the following question: 
-                      'What is the product page link for the {brand} {model} {product_type} and only return the url. 
-                      Prioritize website links from the manufacturer website first and if you cannot find the 
-                      website url from the manufacturer then prioritize URLs from best buy.'
-                      """)
-    # isCorrectURL = input(f'Is this the correct URL?(Y/N):{url}\n')
-    # if isCorrectURL.lower() == 'no' or isCorrectURL.lower() == 'n':
-    #     url = input('Please enter correct url\n')
-    return url
+
 # #This is to format the response from chatgpt to have answers in the right place in the excel workbook
 def format_pedigree_answer(answer):
   #Seperates pedigree answers into a list
@@ -93,7 +171,9 @@ def format_pedigree_answer(answer):
   final_pedigree_specs = "\t".join(final_pedigree_specs_arr)
   
   return final_pedigree_specs
+
 def get_spec_info(brand, product_link):  
+  
   #Get the latest driver of Chrome so that it does not need to be included with source code
   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
   driver.maximize_window()
@@ -110,19 +190,15 @@ def get_spec_info(brand, product_link):
       'final_url': final_url
   }
 
-
-def main(brand, model):
+def main(brand, model, product_link, prev_window):
+  prev_window.destroy()
+  
   #Pedigree questions are held in a seperate file that is not tracked by Git.
   pedigree_questions = None
   with open('./files/pedigree_questions.txt', 'r') as file:
       pedigree_questions = file.read()
   file.close()
-#   product = input("Please enter brand and model\n")
-  #Grabs the brand portion of the string entered by the user
-#   brand = product.split()[0].lower()
-  #Does the same as above but for the model
-#   model = product.split()[1]
-  product_link = get_url(brand, model)
+
   #This was a quick and easy way to get the domain name in the URL
   domain = urlparse(product_link).netloc
   if domain == 'www.bestbuy.com':
@@ -132,81 +208,19 @@ def main(brand, model):
       product_link += '#pdp_specs'
   spec_obj = get_spec_info(brand, product_link)
 
-#   #Get the latest driver of Chrome so that it does not need to be included with source code
-#   driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-#   driver.maximize_window()
-#   driver.get(product_link)
-#   time.sleep(2)
-#   #Get the xpath from the function. Driver needs to be passed for some of the models since we might need to click a button for some of them.
-#   spec_path = brand_logic(brand, driver)
-#   #spec_path[0] = By.XPATH or BY.TAG_NAME, spec_path[1] = element xpath(Reference of html element of specs) or 'body'
-#   product_specs = driver.find_element(spec_path[0], spec_path[1]).get_attribute('textContent')
-#   final_url = driver.current_url
   pedigree_answers = ask_chatgpt(f"""{spec_obj['specs']}\n I have provided some unfiltered data web scraped from the {brand} {model} TV product page. Based off of this data can you answer the following questions and please only provide the answers seperated by commas and exclude the question numbers. 
                                  For each question there will be a list of directions on how to answer each question inside parentheses. Please follow those directions and do not create any data that is not listed in the web scraped data. Here are the questions:\n{pedigree_questions}""")
   #Add the link of the url used to the answers
   pedigree_answers += f', {spec_obj['final_url']}'
   #Format answers
   final_pedigree_specs = format_pedigree_answer(pedigree_answers)
-#   pyperclip.copy(f"{final_pedigree_specs}")
-#   print(final_pedigree_specs)
-  return final_pedigree_specs
-#   print("Specs were copied please paste the results into the tv workbook")
-  #Gives it time so that user can see message
-#   time.sleep(5)
-# main()
+  open_result_window(final_pedigree_specs)
 
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
-import tkinter as tk
-
-root = tk.Tk()
-#Set window size
-root.geometry('300x200')
-brands = [
-    'Samsung',
-    'LG',
-    'Vizio',
-    'TCL',
-    'Sony',
-    'Panasonic',
-    'Hisense',
-    'Roku',
-    'Toshiba'
-]
-    
-    
-tv_questions = ["Screen Size_in","Series","Super Series","Display Type","Minimum Width Of Stand","Miscellaneous In The Box","Native Resolution","Weight_lbs","Overall Width_in","Overall Height_in","Overall Depth_in","Panel Width_in",
-"Panel Height_in","Panel Depth_in","Claimed Has ATSC 3 or Nextgen TV","Claimed Has Variable Refresh Rate","FreeSync claim","G-Sync claim",	
-"Claimed Highest WiFi Standard","Model Year","Total Qty Of HDMI Inputs","Analog Connections","Stereo Audio Output","Digital Audio Output","Ethernet port",	
-"USB-A Ports","USB-C Ports","TV Has Built In Microphone","Can create individual user profiles","Has family settings/parental controls","Has Auto Low Latency Mode",
-"Has reduce blue light feature","Miracast","Airplay","Chromecast","Miscellaneous Features", "Website URL"]
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-search_frame = tk.Frame(root)
-search_frame.grid(row=0, column=0, sticky="nsew")
-search_frame.columnconfigure(0, weight=1)
-brandLabel = Label(search_frame, text = 'Brand')
-brandInput = ttk.Combobox(search_frame, value=brands)
-brandInput.current(0)
-brandInput.config(state='readonly')
-modelLabel = Label(search_frame, text="Model")
-modelInput = Entry(search_frame, width = 23)
-
-brandLabel.grid(row=0,column=0)
-brandInput.grid(row=1,column=0)
-modelLabel.grid(row=2,column=0)
-modelInput.grid(row=3,column=0)
-
-
-
-def open_result_window (brand, model):
-  
+def open_result_window (results):
+    # results = main(brand, model, product_link)
     result_window = tk.Toplevel(root)
     result_window.geometry('600x600')
-    results = main(brand, model)
+    
     results_arr = results.split("\t")
     print(results)
     
@@ -227,61 +241,36 @@ def open_result_window (brand, model):
     copy_button = tk.Button(result_window_frame, text="Copy Specs", command=lambda:pyperclip.copy(results))
     copy_button.grid(column=0, row=1, pady=10)
     
-   
-
-    
     for col in result_tree['column']:
       result_tree.heading(col, text=col)
     i = 0
     while(i < len(tv_questions)):
       result_tree.insert("", "end", values=(tv_questions[i], results_arr[i]))
       i+=1
-      # result_tree.insert("", "end", values=results_arr)
-    # main_frame = Frame(result_window)
-    # main_frame.pack(fill=BOTH, expand=1)
-    # my_canvas = Canvas(main_frame)
-    # my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
-    # my_scrollbar = ttk.Scrollbar(main_frame, orient=VERTICAL, command=my_canvas.yview)
-    # my_scrollbar.pack(side=RIGHT, fill=Y)
-    # my_canvas.configure(yscrollcommand=my_scrollbar.set)
-    # my_canvas.bind('<Configure>', lambda e: my_canvas.configure(scrollregion= my_canvas.bbox("all")))
-    # second_frame = Frame(my_canvas)
-    # my_canvas.create_window((0,0), window=second_frame, anchor="nw")
-    # result_window.grab_set()
-    # for x in range(0, 37):
-    #    questionsLabel =  tk.Label(second_frame, text=tv_questions[x])
-    #    questionsEntry = tk.Entry(second_frame)
-    #    questionsEntry.insert(0, results_arr[x])
-    #   #  questionsLabel.grid(row=0,column=x)
-    #   #  questionsEntry.grid(row=1, column=x)
-    #    questionsLabel.grid(column=0, row = x)
-    #    questionsEntry.grid(column=0, row= x+1)
-    # copy_button = tk.Button(second_frame, text="Copy Specs", command=lambda:pyperclip.copy(results))
-    # copy_button.grid(column=0, row=73)
+      
 
-  
-
-def open_new_window():
-    new_window = tk.Toplevel(root)
-    new_window.geometry('600x100')
-    new_window.title('URL Entry')
-    new_url_label = tk.Label(new_window, text = "Please enter correct URL").grid()
-    new_url_text = tk.Entry(new_window, width=70).grid()
-    enter_url = tk.Button(new_window, text = "Search").grid()
-    new_window.grab_set()
-
-def searchWithBrandModel ():
-    brand = brandInput.get().lower()
-    model = modelInput.get()
-    
-    url = get_url(brand, model)
-    response = messagebox.askyesno("Confirm URL", "Is this the correct URL?\n" + url)
-    if response :
-        open_result_window(brand, model)
-       
-    else:
-        open_new_window()
     
 
-searchButton = tk.Button(search_frame, text="Search", command=searchWithBrandModel).grid(row=4, column=0)
+root = tk.Tk()
+#Set window size
+root.geometry('300x200')
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+search_frame = tk.Frame(root)
+search_frame.grid(row=0, column=0, sticky="nsew")
+search_frame.columnconfigure(0, weight=1)
+brandLabel = Label(search_frame, text = 'Brand')
+brandInput = ttk.Combobox(search_frame, value=brands)
+brandInput.current(0)
+brandInput.config(state='readonly')
+modelLabel = Label(search_frame, text="Model")
+modelInput = Entry(search_frame, width = 23)
+
+brandLabel.grid(row=0,column=0)
+brandInput.grid(row=1,column=0)
+modelLabel.grid(row=2,column=0)
+modelInput.grid(row=3,column=0)
+# loading_window('Searching for model page.')
+searchButton = tk.Button(search_frame, text="Search", command=lambda:searchWithBrandModel(brandInput.get().lower(), modelInput.get())).grid(row=4, column=0, pady=10)
 root.mainloop()
